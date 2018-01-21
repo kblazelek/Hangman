a71a9789b389a4d1f91544599006e858e316a9a7
kblazelek <kblazelek@git.com>
2018-01-21 11:26:11 +0100

    Added splash screen and firebase references

---
+++ app/src/main/java/com/example/traxx/hangman/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.example.traxx.hangman;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBarSplashScreen);
+        progressBar.setVisibility(View.VISIBLE);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
